0fa655e853a9a41939fcec12bc011f51a06c3f25
zinelpro <rhcchfc566@gmail.com>
2025-05-21 15:03:35 +0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+
+mkdir ~/.config
+mkdir ~/Pictures
+cp picom.conf ~/.config
+cp -r xfce4 ~/.config
+cp wallpaper.jpg ~/Pictures
+
+mkdir ~/.themes
+git clone https://github.com/siyms/Gtk-Squared.git
+cp -r Gtk-Squared ~/.themes
+
